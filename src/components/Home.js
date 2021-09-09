7d0f6ebd15812011e32179c456ef84e1447104df
Steve <asapcrafter@gmail.com>
2021-09-08 22:21:43 -0700

    Added homepage background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,15 @@ import Promo from './Promo';
 
 const Home = () => {
     return (
-        <div className='content'>
+        <div className='content' id='home'>
             <div id="home-wrapper">
+                <div className="home-background">
+                    <div id="home-greeting">
+                        <div id="hg-title">Start Your Glow</div>
+                        <div id="hg-description">Discover your personal skincare routine. Explore new seasonal skin, body, and hair care.</div>
+                        <div id="hg-button">SHOP NOW</div>
+                    </div>
+                </div>
                 < Promo />
             </div>
         </div>
