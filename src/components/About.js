c7b146a5342c1d757e1f6e74a6ee2f809e506c14
Steve <asapcrafter@gmail.com>
2021-09-12 16:08:04 -0700

    added scss rem function

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,22 @@ import React from 'react';
 const About = () => {
     return (
         <div className='content' id='about'>
-            about
+            <div className="about-wrapper">
+                <div className="about-background">
+                    <div className="about-title">OUR MISSION</div>
+                </div>
+                <div className="about-text-container">
+                    <div className='about-text'>
+                        One of the biggest problems in the beauty industry is that many brands focus on making products that will sell rather than items that are good for our skin. This leads to exaggerated promises, multi-step procedures, and harsh products that cause long-term skin damage.
+                    </div>
+                    <div className="about-text">
+                        Despite the abundance of product options and information available, we are more confused about skincare than ever before as consumers. Consumers have been taught to apply more product than our skin requires, disrupting the skin's natural functions and potentially leading to chronic inflammation.
+                    </div>
+                    <div className="about-text">
+                        We believe in the power of simplicity and encourage everyone to take a step back, reset, and pay attention to their skin's actual requirements.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
